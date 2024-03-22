e7f3cc708fc63287b2480f18fec2c9be6916d67f
piotrczechowski <czechowp@gmail.com>
2024-03-22 21:59:33 +0100

    [fix]

---
+++ asistant_app/input_error.py
@@ -0,0 +1,11 @@
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please."
+        except KeyError:
+            return "Enter user name."
+        except IndexError:
+            return "Invalid index provided."
+    return inner
